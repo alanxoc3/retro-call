479ad9b05354506acffee91bfe9532deac312e55
Alan Morgan <alanxoc3@gmail.com>
2019-08-23 19:59:10 -0600

    Production branch init commit.
    Yeah, I can now demo my progress to people. In theory...

---
+++ precache-manifest.3b4d30babd85deb931924789ea899812.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e19aeacba4dab7cf53e1c17ff9ebd5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b239680ea22ea4b55b7d",
+    "url": "/static/css/main.7edce0b7.chunk.css"
+  },
+  {
+    "revision": "2df9b245384f7c4887bf",
+    "url": "/static/js/2.11b56f10.chunk.js"
+  },
+  {
+    "revision": "b239680ea22ea4b55b7d",
+    "url": "/static/js/main.874c2660.chunk.js"
+  },
+  {
+    "revision": "bec9565d6bcb44787402",
+    "url": "/static/js/runtime~main.f7767338.js"
+  }
+]);
