3bc7e3fb54c1a48a8e531af24baead2d175e4b9c
Alan Morgan <alanxoc3@gmail.com>
2019-12-06 21:06:04 -0700

    Created a basic text timeout.
    - Also started work on notes. There is a beep now and a basic note map.
    - And I changed the font to a console one and centered it.

--- src/SpeechText.js
+++ src/SpeechText.js
@@ -2,6 +2,34 @@ import React from 'react';
 import './SpeechText.css';
 
 
+function play_note(note) {
+   let note_map = {
+      a: 880.0000,
+      b: 987.7666,
+      c: 523.2511,
+      d: 587.3295,
+      e: 659.2551,
+      f: 698.4565,
+      g: 783.9909,
+      0: 932.3275,
+      1: 830.6094,
+      2: 739.9888,
+      3: 622.2540,
+      4: 554.3653
+   }
+
+   var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+   if (note_map[note]) {
+      // create Oscillator node
+      var oscillator = audioCtx.createOscillator();
+      oscillator.type = 'square';
+      oscillator.frequency.setValueAtTime(note_map[note], audioCtx.currentTime); // value in hertz
+      oscillator.connect(audioCtx.destination);
+      oscillator.start();
+      // end
+   }
+}
+
 class SpeechText extends React.Component {
    constructor() {
       super();
@@ -9,6 +37,7 @@ class SpeechText extends React.Component {
    }
 
    setup = () => {
+      // create web audio api context
       let SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
       this.recognition = new SpeechRecognition();
 
@@ -19,8 +48,13 @@ class SpeechText extends React.Component {
 
       this.recognition.onresult = (e) => {
          var last = e.results.length - 1;
-         var transcript = e.results[last][0].transcript;
-         this.setState({transcript: transcript + "."});
+         var transcript = e.results[last][0].transcript.toUpperCase();
+         this.setState({transcript: transcript});
+         clearTimeout(this.timeout)
+         this.timeout = setTimeout(() => {
+            this.setState({transcript: ''});
+            play_note('a')
+         }, Math.log(this.state.transcript.length+1)*1000)
       }
 
       this.recognition.onerror = (e) => {
